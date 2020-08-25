ad184645b1b45580381b95039442dce1016e667e
QuocVietHa08 <quocvietha08@gmail.com>
2020-08-25 13:30:29 +0700

    example file

---
+++ moreExample/moreExample.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.your-class').slick({
+    // autoplay: true,
+    // autoplaySpeed: 1000,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '.arrow-left',
+    nextArrow: '.arrow-right',
+  });
+});

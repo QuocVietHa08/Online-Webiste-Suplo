d4dd1b33cd510221750db617c7dbf19f34bd272f
QuocVietHa08 <quocvietha08@gmail.com>
2020-09-07 15:12:03 +0700

    tintuc page

---
+++ TinTuc/TinTuc.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('.blog-single-slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: '.slider-btn-angle-left',
+    nextArrow: '.slider-btn-angle-right',
+  });
+});
